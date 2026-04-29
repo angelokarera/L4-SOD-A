a28c47d900e6ea396f198d0d00f6e0a35b84b228
karera <nkusikarera13@gmail.com>
2026-04-29 20:52:25 +0200

    Add dailyLife.js file

---
+++ dailyLife.js
@@ -0,0 +1,18 @@
+const name = "Nkusi Karera Ange";   
+const age = 18;
+const favoriteFood = "rice";
+const studyHoursPerDay = 5;
+const isStudent = true;
+
+const studyHoursPerWeek = studyHoursPerDay * 7;
+const ageInMonths = age * 12;
+const ageInDogYears = age * 7;
+
+console.log("Hello, my name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I love eating " + favoriteFood + ".");
+console.log("I study " + studyHoursPerDay + " hours per day, which is " + studyHoursPerWeek + " hours per week.");
+console.log("I am " + ageInMonths + " months old.");
+console.log("In dog years, I am " + ageInDogYears + ".");
+console.log("Am I a student? " + isStudent);
+ 
